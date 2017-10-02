345462f4be387027cee41731ba56f148d013002c
StMrKirk <31744843+StMrKirk@users.noreply.github.com>
2017-10-02 21:55:26 +0300

    change function get_trending_repositories

--- github_trending.py
+++ github_trending.py
@@ -14,23 +14,12 @@ def get_data_from_github():
 
 
 def get_trending_repositories(top_repos_data, top_list_count):
-    top_list = []
-    for top_number in range(top_list_count+1):
-        repository_name = top_repos_data[top_number]['name']
-        repository_url = top_repos_data[top_number]['html_url']
-        repository_issues = top_repos_data[top_number]['open_issues']
-        repository = (repository_name, repository_url, repository_issues)
-        top_list.append(repository)
-    return top_list
-
-
-def pprint_top_list(top_list):
-    for repo in top_list:
-        print('Name: {:10} URL: {:10} Open Issues: {}'.format(repo[0],repo[1],repo[2]))
+    for repo in range(top_list_count+1):
+        print('Name: {:10} URL: {:10} Open Issues: {}'
+              .format(top_repos_data[repo]['name'], top_repos_data[repo]['html_url'], top_repos_data[repo]['open_issues']))
 
 
 if __name__ == '__main__':
     top_list_count = 20
     top_repos_data = get_data_from_github()
-    top_list = get_trending_repositories(top_repos_data, top_list_count)
-    pprint_top_list(top_list)
+    get_trending_repositories(top_repos_data, top_list_count)
